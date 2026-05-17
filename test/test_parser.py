49accd5ac59b456e462f4795b80f2a979bbeba62
Ledmington <ledmington.dev@gmail.com>
2026-05-17 22:09:07 +0200

    Added more parser tests

--- test/test_parser.py
+++ test/test_parser.py
@@ -6,8 +6,32 @@ from lexer import Lexer
 from parser import parse_program
 
 
+def test_variable_declaration():
+    input = """VAR x;."""
+    expected = Program()
+    actual = parse_program(Lexer(input))
+    assert expected == actual
+
+
 def test_example():
+    # Taken from https://en.wikipedia.org/wiki/PL/0
+    input = """var i, s;
+begin
+  i := 0; s := 0;
+  while i < 5 do
+  begin
+    i := i + 1;
+    s := s + i * i
+  end
+end."""
+    expected = ...
+    actual = parse_program(Lexer(input))
+    assert expected == actual
+
+
+def test_function_declaration():
     input = """VAR x, squ;
+
 PROCEDURE square;
 BEGIN
    squ := x * x
@@ -27,17 +51,109 @@ END."""
     assert expected == actual
 
 
-def test_variable_declaration():
-    input = """VAR x;."""
-    expected = Block(
-        parent=None,
-        gl_sym=None,
-        lc_sym=None,
-        defs=[
-            Definition(symbol=Symbol("x", standard_types["int"])),
-        ],
-        body=StatementList(),
-    )
-    print(expected)
+def test_function_call():
+    # Taken from https://en.wikipedia.org/wiki/PL/0
+    input = """const max = 100;
+var arg, ret;
+
+procedure isprime;
+var i;
+begin
+	ret := 1;
+	i := 2;
+	while i < arg do
+	begin
+		if arg / i * i = arg then
+		begin
+			ret := 0;
+			i := arg
+		end;
+		i := i + 1
+	end
+end;
+
+procedure primes;
+begin
+	arg := 2;
+	while arg < max do
+	begin
+		call isprime;
+		if ret = 1 then write arg;
+		arg := arg + 1
+	end
+end;
+
+call primes
+."""
+    expected = ...
+    actual = parse_program(Lexer(input))
+    assert expected == actual
+
+
+def test_complex():
+    # Taken from https://en.wikipedia.org/wiki/PL/0
+    input = """VAR x, y, z, q, r, n, f;
+
+PROCEDURE multiply;
+VAR a, b;
+BEGIN
+  a := x;
+  b := y;
+  z := 0;
+  WHILE b > 0 DO
+  BEGIN
+    IF ODD b THEN z := z + a;
+    a := 2 * a;
+    b := b / 2
+  END
+END;
+
+PROCEDURE divide;
+VAR w;
+BEGIN
+  r := x;
+  q := 0;
+  w := y;
+  WHILE w <= r DO w := 2 * w;
+  WHILE w > y DO
+  BEGIN
+    q := 2 * q;
+    w := w / 2;
+    IF w <= r THEN
+    BEGIN
+      r := r - w;
+      q := q + 1
+    END
+  END
+END;
+
+PROCEDURE gcd;
+VAR f, g;
+BEGIN
+  f := x;
+  g := y;
+  WHILE f # g DO
+  BEGIN
+    IF f < g THEN g := g - f;
+    IF g < f THEN f := f - g
+  END;
+  z := f
+END;
+
+PROCEDURE fact;
+BEGIN
+  IF n > 1 THEN
+  BEGIN
+    f := n * f;
+    n := n - 1;
+    CALL fact
+  END
+END;;
+  ?x; ?y; CALL multiply; !z;
+  ?x; ?y; CALL divide; !q; !r;
+  ?x; ?y; CALL gcd; !z;
+  ?n; f := 1; CALL fact; !f
+END."""
+    expected = ...
     actual = parse_program(Lexer(input))
     assert expected == actual
